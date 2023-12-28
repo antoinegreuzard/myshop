295a1c77dd0c5885aa2c3801d517328e6d952364
antoinegreuzard <antoine@antoinegreuzard.fr>
2023-12-28 21:40:29 +0100

    Refactor home view component and add product item component
    The HomeView component in `myshop/src/views/HomeView.vue` has been modified to include a new feature. The changes include:
    - Adding a conditional rendering logic to display products or a loading message
    - Importing and using the ProductItem component in the template
    - Introducing a reactive `products` variable with an API call to fetch and populate the products
    
    The new file `myshop/src/components/Products/ProductItem.vue` has been created and includes the following:
    - Template with product details display
    - Script setup with prop definition for `product`
    - Scoped styling for the product item
    
    These modifications improve the functionality of the home view by adding the ability to display and load products.

---
+++ myshop/src/components/Products/ProductItem.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="product-item">
+    <h3>{{ product.name }}</h3>
+    <p>{{ product.description }}</p>
+    <p>Prix : {{ product.price }} €</p>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+defineProps({
+  product: {
+    type: Object,
+    required: true,
+  },
+});
+</script>
+
+<style scoped>
+.product-item {
+  border: 1px solid #ddd;
+  padding: 1em;
+  margin: 1em;
+  border-radius: 8px;
+}
+
+.product-item h3 {
+  margin-top: 0;
+}
+</style>
--- myshop/src/views/HomeView.vue
+++ myshop/src/views/HomeView.vue
@@ -1,12 +1,33 @@
 <template>
   <div class="home">
     <h1>Bienvenue sur MyShop</h1>
-    <p>Ceci est la page d'accueil de votre application e-commerce MyShop.</p>
+    <div v-if="products.length">
+      <h2>Produits Disponibles</h2>
+      <ProductItem v-for="product in products" :key="product.id" :product="product" />
+    </div>
+    <div v-else>
+      <p>Chargement des produits...</p>
+    </div>
   </div>
 </template>
 
 <script setup>
-// Logique JavaScript pour Home.vue
+import { ref, onMounted } from 'vue';
+import ProductItem from '../components/Products/ProductItem.vue';
+
+const products = ref([]);
+
+onMounted(async () => {
+  try {
+    const response = await fetch('http://localhost/api/products');
+    if (!response.ok) throw new Error('Erreur de chargement des produits');
+
+    const data = await response.json();
+    products.value = data['hydra:member'];
+  } catch (error) {
+    console.error(error.message);
+  }
+});
 </script>
 
 <style scoped>
@@ -15,7 +36,7 @@
   padding: 2rem;
 }
 
-.home h1 {
+.home h1, .home h2 {
   margin-bottom: 1rem;
 }
 </style>
