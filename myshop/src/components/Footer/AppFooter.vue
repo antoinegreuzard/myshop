1367f678c35b1c3e598d3413dbb5233248f531b5
antoinegreuzard <antoine@antoinegreuzard.fr>
2023-12-28 20:08:54 +0100

    Update index.html title and change it from "Vite + Vue" to "MyShop". Modify App.vue to import and use Header and Footer components instead of HelloWorld component. Add a new Footer component with its template, logic, and styles. Also, add a new Header component with its template, logic, and styles.

---
+++ myshop/src/components/Footer/AppFooter.vue
@@ -0,0 +1,59 @@
+<template>
+  <footer class="footer">
+    <div class="footer-content">
+      <section class="footer-section">
+        <h3>MyShop</h3>
+        <p>
+        Explorez notre large gamme de produits et profitez d'une expérience d'achat exceptionnelle.
+        </p>
+      </section>
+    </div>
+
+    <div class="footer-bottom">
+      <p>&copy; 2023 MyShop - Tous droits réservés.</p>
+    </div>
+  </footer>
+</template>
+
+<script setup>
+// Logique JavaScript si nécessaire
+</script>
+
+<style scoped>
+.footer {
+  background-color: #333;
+  color: white;
+  text-align: center;
+  padding: 1em 0;
+}
+
+.footer-content {
+  display: flex;
+  justify-content: space-around;
+  padding: 1em;
+}
+
+.footer-section h3 {
+  margin-bottom: 0.5em;
+}
+
+.footer-section ul {
+  list-style: none;
+  padding: 0;
+}
+
+.footer-section ul li a {
+  color: white;
+  text-decoration: none;
+  transition: color 0.3s;
+}
+
+.footer-section ul li a:hover {
+  color: #646cff;
+}
+
+.footer-bottom {
+  margin-top: 1em;
+  font-size: 0.8em;
+}
+</style>
---
+++ myshop/src/components/Header/AppHeader.vue
@@ -0,0 +1,90 @@
+<template>
+    <header class="header">
+        <div class="logo-container">
+            <img :src="logoUrl" alt="MyShop Logo" class="logo"/>
+        </div>
+        <div class="search-container">
+            <input type="text" placeholder="Rechercher des produits..." class="search-input"/>
+            <button class="search-button">Recherche</button>
+        </div>
+        <nav class="navigation">
+            <ul>
+                <li><a href="/categories">Catégories</a></li>
+                <li><a href="/promotions">Promotions</a></li>
+                <li><a href="/about">À propos</a></li>
+                <li><a href="/contact">Contact</a></li>
+            </ul>
+        </nav>
+        <div class="user-actions">
+            <a href="/account" class="account-link">Mon Compte</a>
+        </div>
+    </header>
+</template>
+
+<script>
+import logo from '../../assets/images/logo.png';
+
+export default {
+  data() {
+    return {
+      logoUrl: logo,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1em;
+    background-color: #fff;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+
+.logo-container .logo {
+    height: 50px;
+}
+
+.search-container {
+    display: flex;
+}
+
+.search-input {
+    padding: 0.5em;
+    border: 1px solid #ccc;
+    border-radius: 4px 0 0 4px;
+}
+
+.search-button {
+    padding: 0.5em 1em;
+    border: none;
+    border-radius: 0 4px 4px 0;
+    background-color: #646cff;
+    color: white;
+    cursor: pointer;
+}
+
+.navigation ul {
+    list-style: none;
+    display: flex;
+    margin: 0;
+    padding: 0;
+}
+
+.navigation li a {
+    padding: 0.5em 1em;
+    text-decoration: none;
+    color: #333;
+}
+
+.navigation li a:hover {
+    text-decoration: underline;
+}
+
+.user-actions {
+    display: flex;
+    align-items: center;
+}
+</style>
